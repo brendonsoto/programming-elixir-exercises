3dfbb73037028c4f6b44e5984989ef1fb8bdb062
brendon <brendon.soto@gmail.com>
2020-05-04 11:41:49 -0400

    Completed chapter 17

--- ch17/sequence/lib/sequence/ex1.ex
+++ ch17/sequence/lib/sequence/ex1.ex
@@ -1,6 +1,23 @@
 defmodule Sequence.Ex1 do
   use GenServer
 
+  #####
+  # External API
+  def start_link(contents) do
+    GenServer.start_link(__MODULE__, contents, name: __MODULE__)
+  end
+
+  def pop do
+    GenServer.call(__MODULE__, :pop)
+  end
+
+  def push(val) do
+    GenServer.cast(__MODULE__, {:push, val})
+  end
+
+  #####
+  # Internal API
+
   def init(contents) when is_list(contents) do
     {:ok, contents}
   end
@@ -14,5 +31,9 @@ defmodule Sequence.Ex1 do
 
   def handle_cast({:push, value}, current_contents) do
     { :noreply, [value | current_contents] }
+  end
+
+  def terminate({reason, _state}) do
+    IO.puts reason
   end
 end
